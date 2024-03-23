5a9419d80a596d12f2d779dff8ddf43e0baff11c
nick <nikolay.falaleev@immensus.me>
2024-03-23 02:15:36 +0000

    feat: Add 2 axis fingers control

--- scripts/sender_ue.py
+++ scripts/sender_ue.py
@@ -107,12 +107,12 @@ CONTROLS = {
 
 # Neutral pose for the hand and the arm
 NEUTRAL_POSE: dict[str, tuple[float, float, float]] = {
-    'Thumb 01 R Ctrl': (0.0, -25.0, 10.0),
+    'Thumb 01 R Ctrl': (0.0, -20.0, 10.0),
     'Index 01 R Ctrl': (0.0, 0.0, -14.0),
-    'Index 02 R Ctrl': (0.0, 0.0, -14.0),
+    'Index 02 R Ctrl': (0.0, 7.0, -14.0),
     'Index 03 R Ctrl': (0.0, 0.0, -3.5),
     'Middle 01 R Ctrl': (0.0, 0.0, -12.0),
-    'Middle 02 R Ctrl': (0.0, 0.0, -16.0),
+    'Middle 02 R Ctrl': (0.0, 7.0, -16.0),
     'Middle 03 R Ctrl': (0.0, 0.0, -5.0),
     'Ring 01 R Ctrl': (0.0, 0.0, -14.0),
     'Ring 02 R Ctrl': (0.0, 0.0, -24.0),
@@ -131,20 +131,79 @@ def get_angle_by_3_points(p0: np.ndarray, p1: np.ndarray,
 
     p0p1 = p0-p1
     p2p1 = p2-p1
-    cosine_angle = np.dot(p0p1, p2p1) / \
-        (np.linalg.norm(p0p1) * np.linalg.norm(p2p1))
-    ang = np.degrees(np.arccos(cosine_angle)) - 180.0
+    ang = angle_between_vectors(p0p1, p2p1) - 180.0
     return float(ang)
 
 
-def get_3d_angle(xyz: np.ndarray, keypoints: tuple[str, str, str])\
+def get_ort_plane(plane: np.ndarray, vect: np.ndarray) -> np.ndarray:
+    """Get the ortogonal plane to a given plane, passing through a vector
+
+    Args:
+        plane (np.ndarray): Plane, defined by its normal.
+        vect (np.ndarray): Vector (3 elements).
+
+    Returns:
+        np.ndarray: The resulted plane normal.
+    """
+    norm = np.cross(vect, plane)
+    return norm/np.linalg.norm(norm)
+
+
+def project_vector_onto_plane(vect: np.ndarray, plane: np.ndarray)\
+        -> np.ndarray:
+    """Project a vector onto a plane.
+
+    https://math.stackexchange.com/questions/633181/formula-to-project-a-vector-onto-a-plane#
+
+    Args:
+        vect (np.ndarray): Vector to be projected (as 3 values).
+        plane (np.ndarray): Plane, defined by its norm.
+
+    Returns:
+        np.ndarray: The projection of the vector onto the plane.
+    """
+    proj = vect - np.dot(vect, plane)*plane
+    return proj
+
+
+def angle_between_vectors(v1: np.ndarray, v2: np.ndarray) -> float:
+    """Get the angle between two vectors, assuming the vectors are not
+    collinear and have non-zero length.
+
+    Args:
+        v1 (np.ndarray): First vector (as 3 values).
+        v2 (np.ndarray): Second vector (as 3 values).
+
+    Returns:
+        float: The angle in degrees.
+    """
+    return float(np.degrees(np.arccos(
+        np.dot(v1, v2) / (np.linalg.norm(v1)*np.linalg.norm(v2)))))
+
+
+def get_finger_angle(xyz: np.ndarray, keypoints: tuple[str, str, str],
+                     plane: np.ndarray)\
         -> tuple[float, float, float]:
+    r2 = 0.0
     p0 = xyz[:, HAND_BONES.index(keypoints[0])]
     p1 = xyz[:, HAND_BONES.index(keypoints[1])]
     p2 = xyz[:, HAND_BONES.index(keypoints[2])]
-    z = get_angle_by_3_points(p0, p1, p2)
-    y = 0.0
-    return 0.0, y, z
+    # Apply to proxima joint of the selected fingers
+    if 'Proximal' in keypoints[1] and any(n in keypoints[1]
+                                          for n in ['2', '3', '4', '5']):
+        base_bone = p1 - p0
+        finger_bone = p2 - p1
+        v_plane = get_ort_plane(plane, base_bone)
+        finger_bone_proj = project_vector_onto_plane(finger_bone, plane)
+        finger_bone_proj_v = project_vector_onto_plane(finger_bone, v_plane)
+        r3 = -angle_between_vectors(base_bone, finger_bone_proj_v)
+        # The last term is to deal with edge cases due to unmerical instability
+        r2 = -angle_between_vectors(base_bone, finger_bone_proj)\
+            * (90.0 - abs(r3)) / 90.0
+        print(keypoints, r2, r3)
+    else:
+        r3 = get_angle_by_3_points(p0, p1, p2)
+    return 0.0, r2, r3
 
 
 def get_queued_data(txt: list[str]) -> multiprocessing.Queue:
@@ -161,15 +220,15 @@ def get_queued_data(txt: list[str]) -> multiprocessing.Queue:
         angle, plane = get_hand_orientation(xyz)
         for ctrl, joints in CONTROLS.items():
             if len(joints) == 3:
-                contol_rig[ctrl] = get_3d_angle(xyz, joints)
+                contol_rig[ctrl] = get_finger_angle(xyz, joints, plane)
             # The last joint approximation
             elif len(joints) == 1:
                 if joints[0] in contol_rig:
                     ref = contol_rig[joints[0]]
                     contol_rig[ctrl] = (ref[0]/2.0, ref[1]/2.0, ref[2]/2.0)
             if ctrl == 'Thumb 01 R Ctrl':
-                rot = get_thumb_rotation(xyz, plane)
-                contol_rig[ctrl] = (0.0, -rot/2,  0)
+                r2, r3 = get_thumb_rotation(xyz, plane)
+                contol_rig[ctrl] = (0.0, -r2, -r3)
 
         # Add the wrist pose
         contol_rig['Wrist R Ctrl'] = (angle[0], angle[1], angle[2])
@@ -189,42 +248,54 @@ def get_queued_data(txt: list[str]) -> multiprocessing.Queue:
     return data_queue
 
 
-def get_thumb_rotation(xyz: np.ndarray, plane: np.ndarray) -> float:
-    joints_idx = [HAND_BONES.index(p) for p in
-                  ['RightFinger1Proximal', 'RightFinger1Metacarpal',
-                   'RightFinger2Proximal', 'RightFinger2Metacarpal']]
-    points = np.take(xyz, joints_idx, axis=1)  # (3, 4)
-    svd = np.linalg.svd(points - np.mean(points, axis=1, keepdims=True))
-    normal = svd[0][:, -1]
-    angle = np.degrees(np.arccos(
-        np.dot(plane, normal)/(np.linalg.norm(plane)*np.linalg.norm(normal))))
-    return float(angle)
+def get_thumb_rotation(xyz: np.ndarray, plane: np.ndarray)\
+        -> tuple[float, float]:
+    # joints_idx = [HAND_BONES.index(p) for p in
+    #               ['RightFinger1Proximal', 'RightFinger1Metacarpal',
+    #                'RightFinger2Proximal', 'RightFinger2Metacarpal']]
+    # points = np.take(xyz, joints_idx, axis=1)  # (3, 4)
+    # svd = np.linalg.svd(points - np.mean(points, axis=1, keepdims=True))
+    # normal = svd[0][:, -1]
+    # plane_xz = plane.copy()
+    # plane_xz = np.array([plane_xz[0], 0.0, plane_xz[2]])  # XZ projection
+    # normal_xz = np.array([normal[0], 0.0, normal[2]])  # XZ projection
+    # angle = np.degrees(np.arccos(
+    #     np.dot(plane_xz, normal_xz)/(np.linalg.norm(plane_xz)*np.linalg.norm(normal_xz))))
+    # if angle > 90.0:
+    #     angle = 180.0 - angle
+
+    p0 = xyz[:, HAND_BONES.index('RightFinger1Proximal')]
+    p1 = xyz[:, HAND_BONES.index('RightFinger1Metacarpal')]
+    p2 = xyz[:, HAND_BONES.index('RightFinger2Proximal')]
+    p3 = xyz[:, HAND_BONES.index('RightFinger2Metacarpal')]
+
+    index_bone = p2-p3
+    thumb_bone = p0 - p1
+    v_plane = get_ort_plane(plane, index_bone)
+    thumb_bone_proj = project_vector_onto_plane(thumb_bone, plane)
+    thumb_bone_proj_v = project_vector_onto_plane(thumb_bone, v_plane)
+    r2 = angle_between_vectors(index_bone, thumb_bone_proj_v)
+    r3 = angle_between_vectors(index_bone, thumb_bone_proj)
+    return r2, r3
 
 
 def get_hand_orientation(xyz: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     joints_idx = [HAND_BONES.index(p) for p in
                   ['RightFinger2Proximal', 'RightFinger3Metacarpal',
-                   'RightFinger4Proximal']]
+                   'RightFinger5Proximal']]
     xyz_hand = np.take(xyz, joints_idx, axis=1).T
-    xyz_hand -= xyz_hand[1, :]
+    xyz_hand -= xyz_hand[1, :]  # Define the origin of the hand
     plane = np.cross(xyz_hand[0], xyz_hand[2])
+    plane = plane / np.linalg.norm(plane)
     plane_yz = np.array([0.0, plane[1], plane[2]])
     plane_xz = np.array([plane[0], 0.0, plane[2]])
     plane_xy = np.array([plane[0], plane[1], 0.0])
-    angles = np.array((get_vector_ang(plane_xy, np.array([0, 1, 0])),  # yaw
-                       get_vector_ang(plane_yz, np.array([0, 0, 1])),  # pitch
-                       get_vector_ang(plane_xz, np.array([0, 0, 1]))))  # roll
-    angles[angles > 90] = (180 - angles)[angles > 90]
-    print(f'Angles: {angles}')
-
+    angles = np.array((
+        angle_between_vectors(plane_xy, np.array([0, 1, 0])),  # yaw
+        angle_between_vectors(plane_yz, np.array([0, 0, 1])),  # pitch
+        angle_between_vectors(plane_xz, np.array([0, 0, 1]))))  # roll
+    angles[angles > 90.0] = (180.0 - angles)[angles > 90.0]
     return angles, plane
-
-
-def get_vector_ang(vector: np.ndarray, normal: np.ndarray) -> np.ndarray:
-    # Normals have unit length
-    angle = np.degrees(
-        np.arccos(np.dot(vector, normal)/np.linalg.norm(vector)))
-    return angle
 
 
 if __name__ == "__main__":
@@ -237,6 +308,7 @@ if __name__ == "__main__":
 
     txt = ['prob', 'A', 'L', 'E', 'K', 'S']
     # txt = ['prob', 'N', 'I', 'K', 'O', 'L', 'A', 'Y']
+    # txt = ['prob', 'A', 'B', 'V', 'A']
     data_queue = get_queued_data(txt)
 
     # Create and start the processes
