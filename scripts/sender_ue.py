cac19642e77dd89e01fb93256347ef656cb76abc
nick <nikolay.falaleev@immensus.me>
2024-03-31 02:03:10 +0100

    feat: Add input stream with keywords
    feat: Add skip rate
    refactor: Clean up code

--- scripts/sender_ue.py
+++ scripts/sender_ue.py
@@ -1,5 +1,7 @@
 import time
 import queue
+import warnings
+import threading
 import multiprocessing
 
 import numpy as np
@@ -7,16 +9,18 @@ import requests
 
 from src.control.config import ALL_CTRL
 from scripts.retrive_data import filter_non_zero
+from src.control.sequences import PHRASES
 from src.process_data.utils import char_index_map
 from src.control.control_rig import get_control_rig
+from src.control.language_engine import transform_to_list
 
 FPS = 30
 FRAME_TIME = 1.0 / FPS
+SKIP_FRAMES_RATE = 3
 TIMEOUT = 0.1
-
-
 DATA_PATH = 'data/sequence/unified_data_master.npz'
 data = np.load(DATA_PATH)['data']
+SLEEP_TIME = 0.1  # Sleep time to check if we have a new input
 
 
 def send_data(m_queue: multiprocessing.Queue, url: str):
@@ -44,39 +48,43 @@ def send_data(m_queue: multiprocessing.Queue, url: str):
                 send_time = time.perf_counter() - start_t
                 time.sleep(max(FRAME_TIME-send_time, 0.0))
             else:
-                break
-                # values = generate_linear_interpolation((0., 0., 0.), 100)
-
-                # for val in values:
-                #     m_queue.put(val)
+                time.sleep(SLEEP_TIME)
         except queue.Empty:  # Here queue is a mudule, not a class entity
             pass
         except Exception as e:
-            print(f"Error occurred while sending data: {e}")
+            warnings.warn(f"Error occurred while sending data: {e}")
 
 
-def listen_input(m_queue: multiprocessing.Queue):
+def listen_input(data_queue: multiprocessing.Queue):
     while True:
         try:
             user_input = input("Enter a string: ")
-            for char in user_input:
-                m_queue.put(char)
+            txt_sequence = transform_to_list(user_input, list(PHRASES.keys()))
+            get_queued_data(txt=txt_sequence, data_queue=data_queue)
         except KeyboardInterrupt:
             break
+        time.sleep(SLEEP_TIME)
 
 
-def get_queued_data(txt: list[str]) -> multiprocessing.Queue:
-    data_queue: multiprocessing.Queue = multiprocessing.Queue()
-    assert len(txt) > 1, 'We can show >1 letter only'
+def get_queued_data(txt: list[str], data_queue: multiprocessing.Queue):
+    if len(txt) <= 1:
+        warnings.warn(f"{txt} is not a valid sequence. We support > 1 letters")
+        return
     seqs = []
     for i in range(len(txt)-1):
-        seqs.append(filter_non_zero(
-            data[char_index_map[txt[i]], char_index_map[txt[i+1]], :, :, :]))
+        if txt[i] in PHRASES:
+            seqs.append(PHRASES[txt[i]])
+        elif txt[i] in char_index_map and txt[i+1] in char_index_map:
+            seqs.append(filter_non_zero(
+                data[char_index_map[txt[i]],
+                     char_index_map[txt[i+1]], :, :, :]))
+    if txt[-1] in PHRASES:
+        # Deal with the case when a keyphrase at the end of the sequence
+        seqs.append(PHRASES[txt[i]])
     seq = np.concatenate(seqs, axis=-1)
-    for i in range(0, seq.shape[-1], 2):
+    for i in range(0, seq.shape[-1], SKIP_FRAMES_RATE):
         control_rig = get_control_rig(seq[:, :, i])
         data_queue.put(control_rig)
-    return data_queue
 
 
 if __name__ == "__main__":
@@ -85,27 +93,22 @@ if __name__ == "__main__":
     endpoint_url = "http://localhost:30010/remote/preset/RCP_Hand/function/Set%20Hand%20Pose"
 
     # Create a multiprocessing queue for communication between processes
-    data_m_queue = multiprocessing.Queue()
-
-    txt = ['prob', 'A', 'L', 'E', 'K', 'S']
-    # txt = ['prob', 'N', 'I', 'K', 'O', 'L', 'A', 'Y']
-    # txt = ['prob', 'A', 'B', 'V', 'A']
-    data_queue = get_queued_data(txt)
+    data_queue = multiprocessing.Queue()
 
     # Create and start the processes
-
+    input_thread = threading.Thread(
+        target=listen_input, args=(data_queue,))
+    input_thread.daemon = True
     sender_process = multiprocessing.Process(
         target=send_data, args=(data_queue, endpoint_url))
+    input_thread.start()
     sender_process.start()
 
     try:
         # Join the processes to wait for their completion
-        # input_process.join()
+        input_thread.join()
         sender_process.join()
+
     except KeyboardInterrupt:
         # If the user interrupts the program, terminate the processes
-        # input_process.terminate()
-        sender_process.terminate()
-        # If the user interrupts the program, terminate the processes
-        # input_process.terminate()
         sender_process.terminate()
