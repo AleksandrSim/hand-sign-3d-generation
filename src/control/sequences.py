6a05a1eadb4a6f3951bf6163c4f528d4aa2ed373
nick <nikolay.falaleev@immensus.me>
2024-04-01 01:14:20 +0100

    refactor: Clean up sequences preprocessing

--- src/control/sequences.py
+++ src/control/sequences.py
@@ -5,7 +5,10 @@ import numpy as np
 # TODO We need sequences of shape (3, 19, N), while the prerecorded data is
 # in shape (19, 3, N). This should be updated with the data update.
 PHRASES = {
-    'name': np.moveaxis(
-        np.load('data/sequence/Hello_my_name_is.npz')['data'], 0, 1),
-    'from': np.moveaxis(np.load('data/sequence/i_live_in.npz')['data'], 0, 1)
+    'name': np.load('data/sequence/Hello_my_name_is.npz'),
+    'from': np.load('data/sequence/i_live_in.npz')
 }
+
+# TODO We need sequences of shape (3, 19, N), while the prerecorded data is
+# in shape (19, 3, N). This should be updated with the data update.
+PHRASES = {k: np.moveaxis(v['data'], 0, 1) for k, v in PHRASES.items()}
