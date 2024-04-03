704e610d6a649bb68f45de48988010ec7b1d75ec
Aleksandr Simonyan <46563315+AleksandrSim@users.noreply.github.com>
2024-04-02 21:28:16 -0700

    feat: Add normalized data feature in sender_ue.py

--- scripts/sender_ue.py
+++ scripts/sender_ue.py
@@ -26,6 +26,28 @@ SLEEP_TIME = 0.1  # Sleep time to check if we have a new input
 filters = Filters()
 
 
+def linear_interpolate(start, end, num_steps):
+    # This function will create interpolated frames between start and end frames
+    return np.linspace(start, end, num_steps, axis=2)
+
+
+def interpolate_sequences(data, start_idx, end_idx, next_idx=None):
+    transition_data = data[start_idx, end_idx, :, :, :]
+    non_zero_frames_mask = np.any(transition_data != 0, axis=(0, 1))
+    transition_data = transition_data[:, :, non_zero_frames_mask]    
+
+    if next_idx:
+        next_transition_data = data[end_idx, next_idx, :, :, :]
+
+        start_frame = transition_data[:, :, -1:]  
+        end_frame = next_transition_data[:, :, :1] 
+        interpolated_frames = linear_interpolate(start_frame, end_frame, num_steps=120)
+        
+        interpolated_frames_squeezed = np.squeeze(interpolated_frames, axis=-1)
+        transition_data = np.concatenate((transition_data[:, :, :], interpolated_frames_squeezed), axis=2)
+    return transition_data
+
+
 def send_data(m_queue: multiprocessing.Queue, url: str):
     while True:
         try:
@@ -58,12 +80,16 @@ def send_data(m_queue: multiprocessing.Queue, url: str):
             warnings.warn(f"Error occurred while sending data: {e}")
 
 
-def listen_input(data_queue: multiprocessing.Queue):
+def listen_input(data_queue: multiprocessing.Queue, normalize = True):
     while True:
         try:
             user_input = input("Enter a string: ")
             txt_sequence = transform_to_list(user_input, list(PHRASES.keys()))
-            get_queued_data(txt=txt_sequence, data_queue=data_queue)
+            if not normalize:
+                get_queued_data(txt=txt_sequence, data_queue=data_queue)
+            else:
+                get_queued_data_normalized(txt=txt_sequence, data_queue=data_queue)
+
         except KeyboardInterrupt:
             break
         time.sleep(SLEEP_TIME)
@@ -94,6 +120,40 @@ def get_queued_data(txt: list[str], data_queue: multiprocessing.Queue):
     rigs = [pose for rig in rigs for pose in rig]
     for rig in rigs:
         data_queue.put(rig)
+
+
+def get_queued_data_normalized(txt: list[str], data_queue: multiprocessing.Queue):
+    if len(txt) <= 1:
+        warnings.warn(f"{txt} is not a valid sequence. We support > 1 letters")
+        return
+    seqs: list[tuple[np.ndarray, str]] = []
+    for i in range(len(txt)-1):
+        if txt[i] in PHRASES:
+            seqs.append((PHRASES[txt[i]], txt[i]))
+        elif txt[i] in char_index_map and txt[i+1] in char_index_map:
+            if i < len(txt)-2:
+                interpolated_data = interpolate_sequences(data, 
+                                      char_index_map[txt[i]], char_index_map[txt[i+1]], char_index_map[txt[i+2]])
+                seqs.append((interpolated_data, txt[i]))
+            else:
+                seqs.append((filter_non_zero(
+                    data[char_index_map[txt[i]],
+                        char_index_map[txt[i+1]], :, :, :]), txt[i]))
+
+    if txt[-1] in PHRASES:
+        # Deal with the case when a keyphrase at the end of the sequence
+        seqs.append((PHRASES[txt[i]], txt[-1]))
+    rigs = []
+    for seq in seqs:
+        rig_seq = [get_control_rig(seq[0][:, :, i])
+                   for i in range(0, seq[0].shape[-1], SKIP_FRAMES_RATE)]
+        rig_seq = filters(rig_seq)
+        rigs.append(rig_seq)
+    filters.reset()  # Reset filters after processing the phrase
+    rigs = [pose for rig in rigs for pose in rig]
+    for rig in rigs:
+        data_queue.put(rig)
+
 
 
 if __name__ == "__main__":
