7369f689fd6978fca3cdcd871256d5207a895cad
nick <nikolay.falaleev@immensus.me>
2024-04-14 14:15:45 +0100

    chore: Empty list of phrases since we have no phrases in  English yet

--- src/control/sequences.py
+++ src/control/sequences.py
@@ -2,13 +2,11 @@
 
 import numpy as np
 
-# TODO We need sequences of shape (3, 19, N), while the prerecorded data is
-# in shape (19, 3, N). This should be updated with the data update.
+# Fill the dictionary with prerecorded sequences, for example:
+#      'name': np.load('data/sequence/Hello_my_name_is.npz'),
 PHRASES = {
-    'name': np.load('data/sequence/Hello_my_name_is.npz'),
-    'from': np.load('data/sequence/i_live_in.npz')
 }
 
-# TODO We need sequences of shape (3, 19, N), while the prerecorded data is
-# in shape (19, 3, N). This should be updated with the data update.
+# TODO We need sequences of shape (3, 20, N), while the prerecorded data is
+# in shape (20, 3, N). This should be updated with the data update.
 PHRASES = {k: np.moveaxis(v['data'], 0, 1) for k, v in PHRASES.items()}
